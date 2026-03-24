6bd847513bd6fa0adaef1e56b9f72674cd08b4b2
guilherme Chiquito <guilherme.fidelis2570@gmail.com>
2026-03-24 16:57:19 -0300

    adiciona model

---
+++ src/test/java/com/example/Autor/AutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Autor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
